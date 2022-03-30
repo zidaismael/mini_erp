4f60edfe4e26f951dc51907a50a591b07cb8cd90
ZIDA Ismael <zidaismael@solitechno.com>
2022-03-30 20:27:14 +0000

    adapt index

--- sources/public/index.php
+++ sources/public/index.php
@@ -2,45 +2,39 @@
 declare(strict_types=1);
 
 use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\Micro;
+use Router\ErpRouter;
+use Response\ErpResponse;
 
 error_reporting(E_ALL);
 
 define('BASE_PATH', dirname(__DIR__));
 define('APP_PATH', BASE_PATH . '/app');
 
+
 try {
-    /**
-     * The FactoryDefault Dependency Injector automatically registers
-     * the services that provide a full stack framework.
-     */
-    $di = new FactoryDefault();
+    
+   $container = new FactoryDefault();
+   $app = new Micro($container);
+   
+   include APP_PATH . '/config/services.php';
+    
+   //load config
+   $config = $container->getConfig();
+    
+   include APP_PATH . '/config/loader.php';
+ 
+   //construct Mini-ERP routes
+   $erpRouter=new ErpRouter();
+   $erpRouter->init($app);
+   
+   //set response dependency to output json
+   $container->set('responses',function(){new ErpResponse();});
 
-    /**
-     * Read services
-     */
-    include APP_PATH . '/config/services.php';
-
-    /**
-     * Handle routes
-     */
-    include APP_PATH . '/config/router.php';
-
-    /**
-     * Get config service for use in inline setup below
-     */
-    $config = $di->getConfig();
-
-    /**
-     * Include Autoloader
-     */
-    include APP_PATH . '/config/loader.php';
-
-    /**
-     * Handle the request
-     */
-    $application = new \Phalcon\Mvc\Application($di);
-
-    echo $application->handle($_SERVER['REQUEST_URI'])->getContent();
+   $app->handle(
+       $_SERVER["REQUEST_URI"]
+   );
+   
 } catch (\Exception $e) {
     echo $e->getMessage() . '<br>';
     echo '<pre>' . $e->getTraceAsString() . '</pre>';
