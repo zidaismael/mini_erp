bd2d7057fb9a9405c39dc9a58ba3f42016eba196
ZIDA Ismael <zidaismael@solitechno.com>
2022-03-31 20:27:46 +0000

    create role and user models

---
+++ sources/app/models/Role.php
@@ -0,0 +1,74 @@
+<?php
+
+class Role extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     *
+     * @var string
+     */
+    protected $access_type;
+    
+    public function columnMap()
+    {
+        return [
+            'access_type'  => 'accessType',
+        ];
+    }
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setSchema("mini_erp");
+        $this->setSource("role");
+        $this->hasMany('id', 'User', 'id', ['alias' => 'User']);
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Role[]|Role|\Phalcon\Mvc\Model\ResultSetInterface
+     */
+    public static function find($parameters = null): \Phalcon\Mvc\Model\ResultsetInterface
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Role|\Phalcon\Mvc\Model\ResultInterface|\Phalcon\Mvc\ModelInterface|null
+     */
+    public static function findFirst($parameters = null): ?\Phalcon\Mvc\ModelInterface
+    {
+        return parent::findFirst($parameters);
+    }
+    
+    public function getId(){
+        return (int) $this->id;
+    }
+    
+    public function getName(){
+        return (string) $this->name;
+    }
+
+    public function getAccessType(){
+        return (string) $this->accessType;
+    }
+}
---
+++ sources/app/models/User.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Model;
+
+class User extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $login;
+
+    /**
+     *
+     * @var string
+     */
+    public $password;
+
+    /**
+     *
+     * @var integer
+     */
+    public $role_id;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setSchema("mini_erp");
+        $this->setSource("user");
+        $this->belongsTo('id', '\Role', 'id', ['alias' => 'Role']);
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return User[]|User|\Phalcon\Mvc\Model\ResultSetInterface
+     */
+    public static function find($parameters = null): \Phalcon\Mvc\Model\ResultsetInterface
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return User|\Phalcon\Mvc\Model\ResultInterface|\Phalcon\Mvc\ModelInterface|null
+     */
+    public static function findFirst($parameters = null): ?\Phalcon\Mvc\ModelInterface
+    {
+        return parent::findFirst($parameters);
+    }
+
+}
