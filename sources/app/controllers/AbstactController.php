37f5c405c0cdd289a073eb0c95dad14aaaa42a81
ZIDA Ismael <zidaismael@solitechno.com>
2022-03-31 23:47:23 +0000

    create entries data validation

--- sources/app/controllers/AbstactController.php
+++ sources/app/controllers/AbstactController.php
@@ -2,13 +2,21 @@
 declare(strict_types = 1);
 
 use Phalcon\Http\Request;
+use Phalcon\Validation;
+use Exception\ {
+                ApiException, 
+                CoreException
+};
 
+/**
+ * Controller abstract
+ */
 class AbstactController extends \Phalcon\Mvc\Controller
 {
 
     /**
      * Get Json body request
-     * 
+     *
      * @param array $mandatoryKeys
      *            Mandatory key in json body request
      * @throws \Exception
@@ -16,12 +24,12 @@ class AbstactController extends \Phalcon\Mvc\Controller
      */
     protected function getBody(array $mandatoryKeys = [])
     {
-        $body= json_decode($this->request->getRawBody(),true);
+        $body = json_decode($this->request->getRawBody(), true);
         
-        if(empty($body)){
+        if (empty($body)) {
             return [];
         }
-    
+        
         // check mandatory key
         if (! empty($mandatoryKeys)) {
             $foundKeys = array_intersect($mandatoryKeys, array_keys($body));
@@ -33,15 +41,71 @@ class AbstactController extends \Phalcon\Mvc\Controller
         
         return $body ?? [];
     }
-    
+
     /**
      * Respond to call in JSON
-     * @param int $httpCode
-     * @param mixed $content
+     *
+     * @param int $httpCode            
+     * @param mixed $content            
      */
-    protected function output(int $httpCode=200, $content){
+    protected function output(int $httpCode = 200, $content)
+    {
         $this->response->setStatusCode($httpCode);
         return $this->response->setJsonContent($content);
+    }
+
+    /**
+     * Validate api entries data with principal Phalcon validator
+     *
+     * @see https://docs.phalcon.io/4.0/en/api/phalcon_validation#validation          
+     * @param string $expectedType            
+     * @param mixed $data            
+     * @param mixed $validatorOptions
+     *            Additionnal option of validator (eg: messages, string max length,...)
+     * @return bool @throw Exception\ApiException
+     */
+    protected function validateData(string $expectedType, $data, array $validatorOptions)
+    {
+        $validator = new Validation();
+        $expectedType = strtolower($expectedType);
+        
+        switch ($expectedType) {
+            case 'int':
+                $validatorClassName = 'Digit';
+                break;
+            case 'numeric':
+                $validatorClassName = 'Numericality';
+                break;
+            case 'alphanumeric':
+                $validatorClassName = 'Alnum';
+                break;
+            case 'string':
+                $validatorClassName = 'StringLength';
+                break;
+            case 'date':
+                $validatorClassName = 'Date';
+                break;
+            case 'email':
+                $validatorClassName = 'Email';
+                break;
+            default:
+                throw new CoreException(sprintf("Unknown validation type %s %s", $expectedType, __FUNCTION__));
+                break;
+        }
+
+        $className="Phalcon\Validation\Validator\\$validatorClassName";
+        $validator->add("data",new $className($validatorOptions));
+        
+        $entity=new \stdClass();
+        $entity->data=$data;
+        
+        $messageObject=$validator->validate($entity);
+        
+        if (count($messageObject)>0) {
+            throw new ApiException($messageObject[0], 400);
+        } else {
+            return true;
+        }
     }
 }
 
--- sources/app/controllers/ClientController.php
+++ sources/app/controllers/ClientController.php
@@ -1,31 +1,41 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 class ClientController extends AbstactController
 {
-    
-    public function get()
+
+    public function get($id = null)
     {
-        //no id passed => list 
+        $this->validateData('int', $id, [
+            'message' => "Id must be an integer."
+        ]);
         
+        $clientModel=new Client();
+        // var_dump($clientModel);
+        // no id passed => list
+        if (is_null($id)) {
+            
+        } else {
+            var_dump($id);
+        }
+        // id passed => get one
         
-        //id passed => get one
-        
-        return $this->output(201,['toto' => "sdfsdf"]);
-    }
-    
-    public function post()
-    {
-        $body=$this->getBody(['tot']);
-    }
-    
-    public function put()
-    {
-    }
-    
-    public function delete()
-    {
+        return $this->output(200, [
+            'toto' => "sdfsdf"
+        ]);
     }
 
+    public function post()
+    {
+        $body = $this->getBody([
+            'tot'
+        ]);
+    }
+
+    public function put()
+    {}
+
+    public function delete()
+    {}
 }
 
