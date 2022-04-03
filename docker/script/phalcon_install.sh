caa35f622aad07c86d0bb447fa9bf79059004ad3
ZIDA Ismael <zidaismael@solitechno.com>
2022-04-03 10:22:34 +0000

    rollback to phalcon 4.0.6 due to bug on phalcon 4.1.* on ubuntu 20.04 php7.4

--- docker/script/phalcon_install.sh
+++ docker/script/phalcon_install.sh
@@ -1,4 +1,9 @@
 curl -s https://packagecloud.io/install/repositories/phalcon/stable/script.deb.sh | sudo bash
+
+# Workaround because of bug on phalcon 4.1.* with php7.4 and ubuntu focal => must downgrade and use phalcon 4.0.*
+@see https://github.com/phalcon/cphalcon/discussions/15259
+sed -i 's/focal/bionic/g' /etc/apt/sources.list.d/phalcon_stable.list
 sudo apt-get update
-sudo apt-get install php7.4-phalcon
+sudo apt-get install php7.4-phalcon=4.0.6-975+php7.4;
+
 alias phalcon=/data/www/vendor/phalcon/devtools/phalcon
