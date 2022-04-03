803aed8ffb5960a01af97aac5f558c236acb66ea
ZIDA Ismael <zidaismael@solitechno.com>
2022-04-03 10:21:15 +0000

    fix database init file

--- docker/database/database_init.sh
+++ docker/database/database_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-CURRENT_DIR=$(pwd)
+SCRIPT_BASE_FOLDER="$(dirname "$(readlink -e "$0")")"
 MYSQL_PASSWD="erp"
 mysql -u root -p$MYSQL_PASSWD -h mini_erp_database -e "" 
-mysql -u root -p$MYSQL_PASSWD -h mini_erp_database < $CURRENT_DIR/mini_erp.sql
-mysql -u root -p$MYSQL_PASSWD -h mini_erp_database < $CURRENT_DIR/mini_erp_credentials.sql
+mysql -u root -p$MYSQL_PASSWD -h mini_erp_database < $SCRIPT_BASE_FOLDER/mini_erp.sql
+mysql -u root -p$MYSQL_PASSWD -h mini_erp_database < $SCRIPT_BASE_FOLDER/mini_erp_credentials.sql
