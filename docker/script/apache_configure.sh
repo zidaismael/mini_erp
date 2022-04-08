2d5ab1f1e04cbfa3f022f67f153b18784afcc628
ZIDA Ismael <zidaismael@solitechno.com>
2022-04-08 01:06:28 +0000

    generate phpdoc and add command to do it

--- docker/script/apache_configure.sh
+++ docker/script/apache_configure.sh
@@ -2,5 +2,7 @@
 a2dissite 000-default
 rm -rf /etc/apache2/sites-available/*
 cp /data/vhosts/mini_erp.conf /etc/apache2/sites-available
+cp /data/vhosts/doc_mini_erp.conf /etc/apache2/sites-available
 a2ensite mini_erp
+a2ensite doc_mini_erp
 a2enmod rewrite
